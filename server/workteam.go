43df7e2cf793441cf1ae469ab38a7f03f44ee0a0
ken <block24block@gmail.com>
2023-06-08 13:05:11 +0800

    print worker id

--- server/workteam.go
+++ server/workteam.go
@@ -3,6 +3,8 @@ package server
 import (
 	"context"
 	"errors"
+	"os"
+	"strconv"
 
 	"github.com/benthosdev/benthos/v4/public/service"
 	"github.com/rs/zerolog/log"
@@ -12,14 +14,14 @@ import (
 // workteam 工作组
 type workteam struct {
 	workers []*taskWorker
-	ch      chan struct{}
+	running chan struct{}
 }
 
 // newWorkteam 创建工作组
 func newWorkteam(ctx context.Context, taskslist common.Tasklist, workerCount int) *workteam {
 	team := &workteam{
 		workers: make([]*taskWorker, 0, workerCount),
-		ch:      make(chan struct{}),
+		running: make(chan struct{}),
 	}
 
 	for i := 0; i < workerCount; i++ {
@@ -28,9 +30,9 @@ func newWorkteam(ctx context.Context, taskslist common.Tasklist, workerCount int
 
 	go func() {
 		for _, w := range team.workers {
-			<-w.wait()
+			<-w.running
 		}
-		close(team.ch)
+		close(team.running)
 	}()
 
 	return team
@@ -38,45 +40,42 @@ func newWorkteam(ctx context.Context, taskslist common.Tasklist, workerCount int
 
 // wait 等待worker退出
 func (team *workteam) wait() chan struct{} {
-	return team.ch
+	return team.running
 }
 
 // taskWorker worker
 type taskWorker struct {
 	ctx       context.Context
 	taskslist common.Tasklist
-	idx       int
-	ch        chan struct{}
+	id        string
+	running   chan struct{}
 }
 
 // newTaskWorker 创建worker
 func newTaskWorker(ctx context.Context, idx int, taskslist common.Tasklist) *taskWorker {
-	worker := &taskWorker{taskslist: taskslist, ctx: ctx, idx: idx}
+	hostname, _ := os.Hostname()
+	id := hostname + ":" + strconv.Itoa(os.Getpid()) + ":" + strconv.Itoa(idx)
+	worker := &taskWorker{taskslist: taskslist, ctx: ctx, id: id}
 	worker.loop()
 	return worker
 }
 
-// wait 等待worker退出
-func (worker *taskWorker) wait() chan struct{} {
-	return worker.ch
-}
-
 // logDebug 输出日志
 func (worker *taskWorker) logDebug(str string, v ...any) {
-	log.Debug().Str("mod", "taskWorker").Int("idx", worker.idx).Msgf(str, v...)
+	log.Debug().Str("mod", "taskWorker").Str("id", worker.id).Msgf(str, v...)
 }
 
 // logInfo 输出日志
 func (worker *taskWorker) logInfo(str string, v ...any) {
-	log.Info().Str("mod", "taskWorker").Int("idx", worker.idx).Msgf(str, v...)
+	log.Info().Str("mod", "taskWorker").Str("id", worker.id).Msgf(str, v...)
 }
 
 // loop 轮询取task执行
 func (worker *taskWorker) loop() {
-	worker.ch = make(chan struct{})
+	worker.running = make(chan struct{})
 
 	go func() {
-		defer close(worker.ch)
+		defer close(worker.running)
 
 		for {
 			task, err := worker.taskslist.Read(worker.ctx)
